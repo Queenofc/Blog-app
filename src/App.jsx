245f81420aa7374c9cab2fa4cff8014b0e705638
Queenofc <152055731+Queenofc@users.noreply.github.com>
2024-05-21 20:20:33 +0530

    Added pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,  
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home Page!</div>,
+  },
+]);
+
 function App() {
-  return <div>Hello World!</div>;
+  return(
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
