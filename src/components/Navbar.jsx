3d27c3a38b84389cc994ef4f734ba2baf876fe19
Queenofc <152055731+Queenofc@users.noreply.github.com>
2024-05-26 20:01:30 +0530

    completed home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import Logo from '../img/logo.png' 
 
 const Navbar = () => {
@@ -8,7 +9,31 @@ const Navbar = () => {
         <div className="logo">
         <img src={Logo} alt=""></img>
         </div>
-        <div className="links">links</div>
+        <div className="links">
+          <Link className="link" to ="?/cat=art">
+            <h5>ART</h5>
+          </Link>
+          <Link className="link" to ="?/cat=science">
+            <h5>SCIENCE</h5>
+          </Link>
+          <Link className="link" to ="?/cat=technology">
+            <h5>TECHNOLOGY</h5>
+          </Link>
+          <Link className="link" to ="?/cat=cinema">
+            <h5>CINEMA</h5>
+          </Link>
+          <Link className="link" to ="?/cat=design">
+            <h5>DESIGN</h5>
+          </Link>
+          <Link className="link" to ="?/cat=FOOD">
+            <h5>FOOD</h5>
+          </Link>
+          <span>John</span>
+          <span>Logout</span>
+          <span className="write">
+            <Link className="link" to="/write">Write</Link>
+          </span>
+        </div>
       </div>
     </div>
   )
