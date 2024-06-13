cd47e091bec75f4bd5b8d06969b4e0052a35e6bd
Queenofc <152055731+Queenofc@users.noreply.github.com>
2024-06-13 22:29:59 +0530

    Completed the login and logout buttons

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
-import React,{useState}  from 'react'
+import React,{useContext, useState}  from 'react'
 import { Link,useNavigate} from 'react-router-dom'
 import axios from "axios";
+import { AuthContext } from '../context/authContext';
 
 export const Login = () => {
 
@@ -13,6 +14,7 @@ export const Login = () => {
 
   const navigate=useNavigate()
 
+  const { login }=useContext(AuthContext);
 
 const handleChange = e =>{
   setInputs(prev=>({...prev, [e.target.name]: e.target.value}))
@@ -21,7 +23,7 @@ const handleChange = e =>{
 const handleSubmit = async e =>{
   e.preventDefault()
   try{
-    await axios.post("http://localhost:8800/api/auth/login", inputs);
+    await login(inputs)
     navigate("/");
   }catch(err){
     setError(err.response.data);
